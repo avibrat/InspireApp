91396ca617156a96a5987ca55671b54a24520c71
Avinash Bharat <avi2796@gmail.com>
2017-01-01 12:08:00 +0530

    Initial commit

---
+++ app/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import requests
+import json
+
+import config
+
+app = Flask(__name__)
+
+data_url = None
+headers = {
+    'Content-Type': 'application/json'
+}
+
+if config.DEVELOPMENT:
+    data_url = 'https://data.{}.hasura-app.io'.format(config.PROJECT_NAME)
+    headers['Authorization'] = 'Bearer ' + config.ADMIN_TOKEN
+else:
+    # Make a request to the data API as the admin role for full access
+    data_url = 'http://data.hasura'
+    headers['X-Hasura-Role'] = 'admin'
+    headers['X-Hasura-User-Id'] = '1'
+
+query_url = data_url + '/v1/query'
+
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
