23594bb66caa8fd06f7dddf8c4364c53d669a0cc
Avinash Bharat <avi2796@gmail.com>
2017-01-02 07:57:40 +0530

    added login signup

--- app/server.py
+++ app/server.py
@@ -267,7 +267,8 @@ def post_message():
 @app.route("/profile",methods=["POST"])
 def display_profile():
 	email = call_appropriate_get('email')
-	return email
+	pid=1
+	return select_table('comment',{"pid":int(pid)},"-cid")
 	#return select_table('message',{"to_email" : email},"-mid")
 	
 	
