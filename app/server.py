3f6a23a61f4ffd7b32f1c741df6f052d9c6dc522
Avinash Bharat <avi2796@gmail.com>
2017-01-02 13:43:28 +0530

    added login signup

--- app/server.py
+++ app/server.py
@@ -332,6 +332,7 @@ def getQuestions(data_dict):
 				["Is there a part of the picture you are overlooking?"]
         ]
         response['que'] = incident
+		response['tag'] = var
     return response
         
 #get msg from user and pass msg to wit and get intent and entity
